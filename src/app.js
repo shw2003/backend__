325614cf20b35641ccc29336029b1b08f610cf3a
shweta2003 <shwetakamal4714@gmail.com>
2025-01-17 15:22:30 +0530

    util code

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ app.use(
   }),
 );
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+app.use(express.json({ limit: "16kb" })); //accepting the json and limit
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //url
+app.use(express.static("public")); //public folder for image and favicon
 app.use(cookieParser());
 export { app };
