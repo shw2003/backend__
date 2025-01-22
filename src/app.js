b4ef1469e134345921795f2b8837bb1b46cbe8a3
shweta2003 <shwetakamal4714@gmail.com>
2025-01-22 15:34:04 +0530

    router

--- src/app.js
+++ src/app.js
@@ -15,4 +15,11 @@ app.use(express.json({ limit: "16kb" })); //accepting the json and limit
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //url
 app.use(express.static("public")); //public folder for image and favicon
 app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/users", userRouter);
+
 export { app };
