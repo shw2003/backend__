91ae853d31d42688f8ec86a7c6b27b89f0a855d6
shweta2003 <shwetakamal4714@gmail.com>
2025-01-22 16:00:41 +0530

    postman debug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async (res, req) => {
+const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({
-    message: "OK",
+    message: "coffee",
   });
 });
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,6 @@
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
-      next(error),
-    );
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((e) => next(e));
   };
 };
 
@@ -18,3 +16,11 @@ export { asyncHandler };
 //     });
 //   }
 // });
+
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//   };
+// };
+
+// export { asyncHandler };
